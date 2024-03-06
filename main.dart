5789a8978510729f0e9f3ace491ae84212c609f6
BhaveshKukreja29 <146556002+BhaveshKukreja29@users.noreply.github.com>
2024-03-06 21:38:51 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Pro Engineer',
       theme: ThemeData(
         primarySwatch: Colors.blue,
